bdbeac7f062458f77dc3197ad983a944bd907716
MapleInSpring <yifeng@resnet-49-123.resnet.ucsb.edu>
2014-02-23 10:10:53 -0800

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Yifeng Hou on 23/2/14.
+//  Copyright (c) 2014 GraceHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Yifeng Hou on 23/2/14.
+//  Copyright (c) 2014 GraceHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
